b6beec70edab5e8164f20dce70b9a0d47ab9126b
retrydp <thetemon@gmail.com>
2021-05-23 16:07:40 +0300

    initial

---
+++ e-comm/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../scss/componets/_home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__offer-banner">
+        <div className="home__wrapper">
+          <div className="home__offer-text">Super Flash Sale 50% Off</div>
+          <div className="home__offer-items">
+            <div className="home__banner-item banner-cart"></div>
+            <div className="home__banner-item banner-cart"></div>
+            <div className="home__banner-item banner-cart"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
