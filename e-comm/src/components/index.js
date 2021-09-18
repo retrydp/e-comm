500626429e4f08363926e973aa5c2d488bdfcbe5
retrydp <thetemon@gmail.com>
2021-09-18 16:36:23 +0300

    remove non-ts version

--- e-comm/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from "./Header";
-export { default as HeadNavigation } from "./HeadNavigation";
-export { default as Home } from "./Home";
-export { default as BestSeller } from "./BestSeller";
-export { default as Footer } from "./Footer";
-export { default as ProductList } from "./ProductList";
