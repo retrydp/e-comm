7924cc5196a2ce37dc7a0872d723a1226246b305
retrydp <thetemon@gmail.com>
2022-01-16 17:45:56 +0200

    fix

--- e-comm-ts/components/index.tsx
+++ e-comm-ts/components/index.tsx
@@ -1,6 +1,5 @@
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
-export { default as ProductCart } from './ProductCart';
 export { default as Product } from './Product';
 export { default as Layout } from './Layout';
 export { default as NavigationBar } from './NavigationBar';
