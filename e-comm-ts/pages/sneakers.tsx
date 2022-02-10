bdf6a307f9ad5c65542b96db8795dbb88bfed758
retrydp <thetemon@gmail.com>
2022-02-10 10:52:48 +0200

    test

--- e-comm-ts/pages/sneakers.tsx
+++ e-comm-ts/pages/sneakers.tsx
@@ -4,6 +4,7 @@ import { GoodsProps } from '../utils/types';
 import db from '../utils/database';
 import Product from '../models/Product';
 import { GetServerSideProps } from 'next';
+import data from '../utils/data';
 
 const Sneakers: React.FC<GoodsProps> = ({ goods }) => {
   return (
@@ -16,14 +17,17 @@ const Sneakers: React.FC<GoodsProps> = ({ goods }) => {
 export default Sneakers;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  await db.connect();
-  const productDocs = await Product.find({
-    category: 'shoes',
-  }).lean();
-  await db.disconnect();
+  // await db.connect();
+  // const productDocs = await Product.find({
+  //   category: 'shoes',
+  // }).lean();
+  // await db.disconnect();
 
-  const products = productDocs.map(db.convertDocToObj);
+  // const products = productDocs.map(db.convertDocToObj);
+  // return {
+  //   props: { goods: products },
+  // };
   return {
-    props: { goods: products },
+    props: { goods: data },
   };
 };
