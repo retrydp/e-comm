faf9df7abb775a278f1b9c52452c79a58009be77
retrydp <thetemon@gmail.com>
2022-11-23 12:25:24 +0200

    fix

--- __tests__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Contacts from '../pages/contacts';
-import '@testing-library/jest-dom';
-import react from 'react';
-describe('Home', () => {
-  it('renders a heading', () => {
-    expect(true).toBe(true);
-  });
-});
